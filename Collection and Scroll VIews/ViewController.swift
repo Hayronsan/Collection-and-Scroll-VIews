d2c73f5cc28b5cf5a9e2ecbeda2107891a19c6e2
Hayronsan <hayronsan@gmail.com>
2024-04-13 10:14:25 +0500

    Initial Commit

---
+++ Collection and Scroll VIews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection and Scroll VIews
+//
+//  Created by Nodir on 13/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
