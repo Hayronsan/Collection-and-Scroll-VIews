b5ff9902e8113793329771a4d95d7eb8ecbd7137
Hayronsan <hayronsan@gmail.com>
2024-04-13 12:10:14 +0500

    collection view

---
+++ Collection and Scroll VIews/Cells/ItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCollectionViewCell.swift
+//  Collection and Scroll VIews
+//
+//  Created by Nodir on 13/04/24.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Collection and Scroll VIews/Model/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  Collection and Scroll VIews
+//
+//  Created by Nodir on 13/04/24.
+//
+
+import Foundation
